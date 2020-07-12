d131084303323c57221c79ac59b26023388f45fa
ketan-16 <51126671+ketan-16@users.noreply.github.com>
2020-07-12 15:01:06 +0530

    - Banner

--- js/main.js
+++ js/main.js
@@ -1,109 +1,60 @@
-/*  ---------------------------------------------------
-    Template Name: Sona
-    Description: Sona Hotel Html Template
-    Author: Colorlib
-    Author URI: https://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
+$(window).on("load", function () {
+  $(".loader").fadeOut();
+  $("#preloder").delay(200).fadeOut("slow");
+});
 
-'use strict';
+/*------------------
+      Background Set
+  --------------------*/
+$(".set-bg").each(function () {
+  var bg = $(this).data("setbg");
+  $(this).css("background-image", "url(" + bg + ")");
+});
 
-(function ($) {
+//Offcanvas Menu
+$(".canvas-open").on("click", function () {
+  $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+  $(".offcanvas-menu-overlay").addClass("active");
+});
 
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
+$(".canvas-close, .offcanvas-menu-overlay").on("click", function () {
+  $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+  $(".offcanvas-menu-overlay").removeClass("active");
+});
 
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
+// Search model
+$(".search-switch").on("click", function () {
+  $(".search-model").fadeIn(400);
+});
 
-    //Offcanvas Menu
-    $(".canvas-open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").addClass("active");
-    });
+$(".search-close-switch").on("click", function () {
+  $(".search-model").fadeOut(400, function () {
+    $("#search-input").val("");
+  });
+});
 
-    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
+/*------------------
+  Navigation
+--------------------*/
+// $(".mobile-menu").slicknav({
+//   prependTo: "#mobile-menu-wrap",
+//   allowParentLinks: true,
+// });
 
-    // Search model
-    $('.search-switch').on('click', function () {
-        $('.search-model').fadeIn(400);
-    });
+// $(document).ready(function () {
+//   // SideNav Button Initialization
+//   $(".button-collapse").sideNav2();
+//   // SideNav Scrollbar Initialization
+//   var sideNavScrollbar = document.querySelector(".custom-scrollbar");
+//   var ps = new PerfectScrollbar(sideNavScrollbar);
+// });
 
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
-            $('#search-input').val('');
-        });
-    });
+$(".collapse").on("show.bs.collapse", function () {
+  $(".dropdownIcon").replaceWith('<i class="fa fa-angle-up dropdownIcon"></i>');
+});
 
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
-    });
-
-    /*------------------
-        Hero Slider
-    --------------------*/
-   $(".hero-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        mouseDrag: false
-    });
-
-    /*------------------------
-		Testimonial Slider
-    ----------------------- */
-    $(".testimonial-slider").owlCarousel({
-        items: 1,
-        dots: false,
-        autoplay: true,
-        loop: true,
-        smartSpeed: 1200,
-        nav: true,
-        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
-    });
-
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
-
-    /*------------------
-		Date Picker
-	--------------------*/
-    $(".date-input").datepicker({
-        minDate: 0,
-        dateFormat: 'dd MM, yy'
-    });
-
-    /*------------------
-		Nice Select
-	--------------------*/
-    $("select").niceSelect();
-
-})(jQuery);
+$(".collapse").on("hide.bs.collapse", function () {
+  $(".dropdownIcon").replaceWith(
+    '<i class="fa fa-angle-down dropdownIcon"></i>'
+  );
+});
